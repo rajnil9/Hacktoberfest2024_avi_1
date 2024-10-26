0785547f893bc743f7a2cd3781cc00b20c8922eb
Rajnil Saha <rajnilhere@gmail.com>
2024-10-27 01:25:18 +0530

    Create Reverse.java
    Reverse Number in Java

---
+++ Reverse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class a1
+{  
+public static void main(String[] args)   
+{  
+int number,reverse=0;
+Scanner sc=new Scanner(System.in);
+System.out.ptintln("Enter a number");
+number=sc.nextInt();
+while(number!= 0)   
+{  
+int remainder=number%10;  
+reverse=reverse*10+remainder;  
+number=number/10;  
+}  
+System.out.println("The reverse of the given number is: " + reverse);  
+}  
+}  
